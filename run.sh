794812304cb730381e2290fbc087af04424d7c13
todpig <luccasraffael6@gmail.com>
2024-12-09 15:16:03 -0300

    if not exists compile file

--- run.sh
+++ run.sh
@@ -1,8 +1,9 @@
-echo "compiling..."
-
-# compile the code
-g++ *.cpp -o tree -std=c++17
-
+# if not exists comile file
+if [ ! -f "tree" ]; then
+    echo "compiling..."
+    # compile the code
+    g++ *.cpp -o tree -std=c++17
+fi
 # run the code
 echo "running..."
 ./tree
